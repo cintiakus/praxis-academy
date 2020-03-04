446dc149c959a289aada798a7583d4626fefdf11
cintiakus <61131056+cintiakus@users.noreply.github.com>
2020-03-04 12:57:57 +0800

    pertemuan 2

---
+++ Novice/01-02/Latihan/fornstatement.js
@@ -0,0 +1,8 @@
+function dump_props(obj, obj_name) {
+  let result = '';
+  for (let i in obj) {
+    result += obj_name + '.' + i + ' = ' + obj[i] + '<br>';
+  }
+  result += '<hr>';
+  return result;
+}
