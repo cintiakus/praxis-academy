925e2924d6f0b7cc796c0b2392feadabc06cd39a
cintiakus <61131056+cintiakus@users.noreply.github.com>
2020-03-08 18:00:16 +0800

    pertemuan 5

---
+++ Novice/01-05/latihan/latihan2_unique_array.js
@@ -0,0 +1,9 @@
+function unique(arr) {
+    /* your code */
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-0"
+];
+
+alert( unique(values)); // Hare, Krishna, :-0
---
+++ Novice/01-05/latihan/latihan4_create_a_date.js
@@ -0,0 +1,2 @@
+let d = new Date(2020, 1, 20, 3, 12);
+alert( d );
