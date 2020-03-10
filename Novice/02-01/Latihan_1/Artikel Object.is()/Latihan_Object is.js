ce22c8bf0644aff451b4e84f98717856c9e66c5b
cintiakus <61131056+cintiakus@users.noreply.github.com>
2020-03-11 04:23:39 +0800

    Pertemuan 6

---
+++ Novice/02-01/Latihan_1/Artikel Object.is()/Latihan_Object is.js
@@ -0,0 +1,17 @@
+Object.is('foo', 'foo');     // true
+Object.is(window, window);   // true
+
+Object.is('foo', 'bar');     // false
+Object.is([], []);           // false
+
+var foo = { a: 1 };
+var bar = { a: 1 };
+Object.is(foo, foo);         // true
+Object.is(foo, bar);         // false
+
+Object.is(null, null);       // true
+
+// Special Cases
+Object.is(0, -0);            // false
+Object.is(-0, -0);           // true
+Object.is(NaN, 0/0);         // true
